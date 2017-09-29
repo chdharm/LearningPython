2ec7cd2b304a5f1697ceacc4e287b89af24e797d
chdharmtravelyaari <dharmpal.chaudhary@travelyaari.com>
2017-09-30 00:13:41 +0530

    added Basic/CountersInPython.py

---
+++ Basic/CountersInPython.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+print Counter(['B','B','C','B','A','C','A','C','A'])
+print Counter({'A':3,'B':3,'C':3})
+print Counter(A=3,B=3,C=3)
+
+
+
+#OUTPUT
+  #Counter({'A': 3, 'C': 3, 'B': 3})
+  #Counter({'A': 3, 'C': 3, 'B': 3})
+  #Counter({'A': 3, 'C': 3, 'B': 3})
