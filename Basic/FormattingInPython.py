3ee4484448d53e6fb534c662f13dce4a0f542070
chdharmtravelyaari <dharmpal.chaudhary@travelyaari.com>
2017-10-02 19:37:11 +0530

    added Basic/FormattingInPython.py

---
+++ Basic/FormattingInPython.py
@@ -0,0 +1,10 @@
+#For Integer
+print "Hi I am your queen and my  size is  %d" %5
+#For Float
+print "Hi I am your queen and my  size is  %f" %5.5
+#For String
+print "Hi I am your queen and my size is  %s" %"Five-Feets"
+#For Hexadecimal
+print "Hi I am your queen and my size is %x " %0x5
+#For Octal
+print "Hi I am your queen and my size is %o " %05
