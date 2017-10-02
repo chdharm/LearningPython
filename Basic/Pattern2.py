f16e5ff2486dd4d01e1034546956c25b0ca11541
chdharmtravelyaari <dharmpal.chaudhary@travelyaari.com>
2017-10-02 15:58:34 +0530

    Added Basic/Pattern2.py

---
+++ Basic/Pattern2.py
@@ -0,0 +1,20 @@
+def pypart(n):
+    for i in range(0,n):
+        for j in range(0,n):
+            if j<n-i-1:
+                print " ",
+            else:
+                print "*",
+        print
+
+pypart(5)
+
+
+
+
+C:\Python27\python.exe C:/Users/Admin/PycharmProjects/FirstProject/Third.py
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
